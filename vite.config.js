88e52d5c7d5ae7c8fb118d91b539681517bef71c
juddrain0110 <juddrain486@gmail.com>
2025-10-12 16:04:31 +0900

    vite 변경

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/ty-wedding-invite/",
+  base: "/",
   plugins: [react()],
-})
+});
